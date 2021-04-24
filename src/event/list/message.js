8dc5f8f8b8d2ad1d5638b54a740aa741214e7767
Glitch (hello-express) <none>
2021-04-24 14:44:15 +0000

    👠🌪 Checkpoint ./src/event/list/message.js:31004912/1152 ./src/other/clientNavigation.js:31004912/1976

--- src/event/list/message.js
+++ src/event/list/message.js
@@ -3,8 +3,30 @@ module.exports = {
   execute: async (message, client) => {
     let config = client.config;
     
+    try {
+      
+      const findGuildDatabase = await client.Database.Guild.findOne({ID: message.guild.id});
+      if (!findGuildDatabase) {
+        
+        let newData = await client.Database.Guild.Create(message);
+        
+        message.guild.database = newData;
+        
+      } else if (findGuildDatabase) {
+        
+        message.guild.database = findGuildDatabase;
+        
+      }
+      
+    } catch (e) {
+      return console.log(`[ERROR] ${e}`)
+    }
     
-    if (message.author.bot) return console.log(`[DISCORD] cannot response to bot`)
+    if (!message.content.startsWith(message.guild.database.prefix)) return;
+    
+    if (message.author.bot) return;
+    
+    const args = message.content.slice(message.guild.database.prefix.length).trim().split(+)
     
   }
 }
--- src/other/clientNavigation.js
+++ src/other/clientNavigation.js
@@ -1,7 +1,42 @@
 const config = require("./config.js");
+const Guild = require("../mongodb/schemas/Guild.js");
 
-module.exports = async (client) => {
-  
+module.exports = async client => {
   client.config = config;
-  
+  client.Database.Guild = Guild;
+  client.Database.Guild.Create = async function(message) {
+    let newData = new Guild({
+      ID: message.guild.id.toString(),
+      Danger: {
+        Banned: false
+      },
+      Settings: {
+        Prefix: config.prefix,
+        DisabledCommands: []
+      },
+      Statistics: {
+        CommandsUsed: 0
+      },
+      CustomCommands: [],
+      Leveling: []
+    });
+    
+    newData.save();
+    
+    return {
+      ID: message.guild.id.toString(),
+      Danger: {
+        Banned: false
+      },
+      Settings: {
+        Prefix: config.prefix,
+        DisabledCommands: []
+      },
+      Statistics: {
+        CommandsUsed: 0
+      },
+      CustomCommands: [],
+      Leveling: []
+    };
+  };
 };
