ef045301e2f95ce0e0ce302aa1313dca0c7a5de7
Glitch (hello-express) <none>
2021-04-27 15:51:50 +0000

    💗🕌 Checkpoint ./src/music/newTrack.js:31004912/308 ./src/music/newServer.js:31004912/239

---
+++ src/music/newServer.js
@@ -0,0 +1,43 @@
+class ServerQueue {
+  constructor(message, song) {
+    this.message = message;
+    this.query = song;
+  }
+  
+  skip(message) {
+    
+  }
+  
+  add(message, song) {
+    
+  }
+  
+  search(message, song) {
+    
+  }
+  
+  pause(message) {
+    
+  }
+  
+  resume(message) {
+    
+  }
+  
+  stop() {
+    
+  }
+  
+  shuffle() {
+    
+  }
+  
+  nowPlaying() {
+    
+  }
+  
+  removeTrack(num) {
+    
+  }
+  
+}
