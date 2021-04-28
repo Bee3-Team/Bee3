b6edc645b588a5d98fda567fcbee20236bb72139
Glitch (hello-express) <none>
2021-04-28 14:48:53 +0000

    🏊🏼⛄️ Checkpoint ./src/website/views/status/queue.ejs:31004912/10121 ./src/client/index.js:31004912/498 ./src/music/newServer.js:31004912/9

--- src/client/index.js
+++ src/client/index.js
@@ -16,6 +16,19 @@ require("../website/app.js")(client);
 
 const Guild = require("../mongodb/schemas/Guild.js");
 
+// GLOBAL
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours + ':' + minutes + ':' + seconds;
+}
+
 //
 client.Guild = Guild;
 client.Guild.Create = async function a(message, id = false) {
