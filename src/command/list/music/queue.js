3b6a831ac9591d7b083c23a42095aab4603a8fa0
Glitch (hello-express) <none>
2021-04-28 15:22:50 +0000

    🏚🗺 Checkpoint ./src/website/views/queue.ejs:31004912/81 ./src/command/list/music/queue.js:31004912/1258

---
+++ src/command/list/music/queue.js
@@ -0,0 +1,20 @@
+const { ServerQueue } = require("../../../music/newServer.js");
+
+module.exports = {
+  name: "queue",
+  description: "Get songs list",
+  aliases: ["q"],
+  permissions: {
+    user: [],
+    client: []
+  },
+  cooldown: 5,
+  run: async (message, args, client) => {
+    
+    const serverQueue = client.music.get(message.guild.id);
+    if (!serverQueue) return message.channel.send(`There is not songs.`);
+    
+    serverQueue.control.list(false, message);
+    
+  }
+}
