1bdadb841fd2093c01362599758810feeae7d153
Glitch (hello-express) <none>
2021-04-28 05:41:12 +0000

    🛢⛲️ Checkpoint ./src/music/trackManager.js:31004912/2984

--- src/music/trackManager.js
+++ src/music/trackManager.js
@@ -1,3 +1,10 @@
+const config = require("../other/config.js");
+const { ServerEvent } = require("./musicEvents.js");
+const { client } = require("../client/index.js");
+const ytdl = require("ytdl-core");
+const YouTubeAPI = require("simple-youtube-api");
+const youtube = new YouTubeAPI(config.yt);
+
 class trackManager {
   constructor() {
     
@@ -31,6 +38,25 @@ class trackManager {
     
     this.serverQueue = message.client.music.get(message.guild.id);
     
+    if (!songAns) {
+      setTimeout(() => {
+        if (this.serverQueue.connection.dispatcher && message.guild.me.voice.channel) return;
+        this.serverQueue.channel.voice.leave();
+        this.serverQueue.channel.text.send(`I leave from voice channel because no inactive song in queue.`)
+      }, 30000)
+      this.serverQueue.channel.text.send(`Queue ended.`);
+      return message.client.music.delete(message.guild.id);
+    }
+    
+    let stream = null, streamType = songAns.url.includes("youtube.com") ? "opus" : "ogg/opus";
+    
+    try {
+      
+    } catch (e) {
+      if (songAns.url.includes("youtube.com")) {
+        stream = await ytdl(songAns.url, { highWaterMark: 1 << 25 });
+        }  
+    }
   }
 }
 
