9ccfb4be09d4fb661af648435d5c16270e1f6785
Glitch (hello-express) <none>
2021-04-29 08:41:20 +0000

    👛🌗 Checkpoint ./src/music/newServer.js:31004912/1776

--- src/music/newServer.js
+++ src/music/newServer.js
@@ -1,4 +1,4 @@
-const DisTube = require("distube");
+const { Player } = require('discord-player');
 const { MusicManager } = require("./musicManager.js");
 
 class Music extends MusicManager {
@@ -9,6 +9,36 @@ class Music extends MusicManager {
   }
   
   async createClient(client) {
+    client.music = new Player(client);
+    
+    this.createEvent(client);
+  }
+  
+  async createEvent(client) {
+    
+    client.music.on("botDisconnect", async (message) => {
+      message.channel.send("I have been kicked from voice channel.")
+    });
+    
+    client.music.on("channelEmpty", async (message, queue) => {
+      message.channel.send("The voice channel is empty, stopped the music.")
+    });
+    
+    client.music.on("error", async (error, message) => {
+    switch (error) {
+        case 'NotPlaying':
+            message.channel.send("There is no songs.");
+            break;
+        case 'NotConnected':
+            message.channel.send("Please join a voice channel.");
+            break;
+        case 'UnableToJoin':
+            message.channel.send("I do not have permissio");
+            break;
+        default:
+            message.channel.send(`${client.emotes.error} - Something went wrong ... Error : ${error}`);
+    };      
+    })
     
   }
 }
