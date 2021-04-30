35d295f5ce7701000f130ce489abd1c69bb7d39f
Glitch (hello-express) <none>
2021-04-30 15:21:15 +0000

    🚿🌥 Checkpoint ./src/music/Create.js:31004912/3587

--- src/music/Create.js
+++ src/music/Create.js
@@ -1,33 +1,33 @@
 const MusicRoutes = require("./Routes.js");
-const ytdl = require('ytdl-core');
+const ytdl = require("ytdl-core");
 const MusicConfig = require("./Config.js");
 
 class CreateMusic extends MusicRoutes {
   constructor(client, option) {
     super();
-    
+
     this.option = option;
     this.queue = new Map();
     this.client = client;
   }
-  
+
   getQueue(id) {
     return this.queue.get(id) || null;
   }
-  
+
   getGuild(id) {
     return this.client.guilds.cache.get(id) || undefined;
   }
-  
-  async handle(voiceChannel, id, query) {    
+
+  async handle(voiceChannel, id, query) {
     let guild = await this.getGuild(id);
     if (!guild) throw new TypeError("Cannot find this guild.");
-    
+
     let check = await this.getQueue(id);
     if (check) throw new TypeError("This server is playing songs.");
-    
+
     let song 
-    
+
     const Constructor = {
       connection: null,
       songs: [],
@@ -35,13 +35,28 @@ class CreateMusic extends MusicRoutes {
       playing: true,
       loop: false
     };
-    
+
     this.queue.set(id, constructor);
-    
   }
-  
-  async VideoQueue(query) {
-    
+
+  async VideoPlaying(query) {
+    if (!query) throw new Error("Need a query to search song.")
+  }
+
+  async validateVideoURL(url) {
+    var p = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+    if (url.match(p)) {
+      return true;
+    }
+    return false;
+  }
+
+  async validatePlaylistURL(url) {
+    var p = /^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/;
+    if (url.match(p)) {
+      return true;
+    }
+    return false;
   }
 }
 
