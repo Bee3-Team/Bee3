d5cdd53ab24ffc8639c91c055d3d4fbd8706d2ed
Glitch (hello-express) <none>
2021-04-30 15:09:15 +0000

    🍳🍨 Checkpoint ./src/music/Create.js:31004912/1748

--- src/music/Create.js
+++ src/music/Create.js
@@ -8,18 +8,41 @@ class CreateMusic extends MusicRoutes {
     
     this.option = option;
     this.queue = new Map();
+    this.client = client;
   }
   
   getQueue(id) {
     return this.queue.get(id) || null;
   }
   
-  async createAndPlay(voiceChannel, id, query) {
-    const Constructor = {
-      
-    };
+  getGuild(id) {
+    return this.client.guilds.cache.get(id) || undefined;
   }
   
+  async handle(voiceChannel, id, query) {    
+    let guild = await this.getGuild(id);
+    if (!guild) throw new TypeError("Cannot find this guild.");
+    
+    let check = await this.getQueue(id);
+    if (check) throw new TypeError("This server is playing songs.");
+    
+    let song 
+    
+    const Constructor = {
+      connection: null,
+      songs: [],
+      volume: this.option.volume,
+      playing: true,
+      loop: false
+    };
+    
+    this.queue.set(id, constructor);
+    
+  }
+  
+  async VideoQueue(query) {
+    
+  }
 }
 
 module.exports = CreateMusic;
