0454c50bc03ce9360878f09fc4521dde5693d1c3
Glitch (hello-express) <none>
2021-05-16 11:12:49 +0000

    🌖👮 Checkpoint ./src/website/views/part/head.ejs:31004912/30 ./src/command/list/general/help.js:31004912/14 ./src/other/Activity.js:31004912/25 ./src/event/list/ready.js:31004912/3 ./src/website/views/home.ejs:31004912/14

--- src/command/list/general/help.js
+++ src/command/list/general/help.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "Show all existing commands",
   aliases: ["h"],
   permissions: {
-    user: ["MANAGE_GUILD"],
+    user: [],
     client: []
   },
   cooldown: 5,
--- src/other/Activity.js
+++ src/other/Activity.js
@@ -1,4 +1,4 @@
 module.exports = {
-  name: "my dashboard",
+  name: "aplha release",
   type: "playing"
 }
