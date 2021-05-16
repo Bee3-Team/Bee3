66517b9bb0b5e689d5b200beafc5e34f1ada6a91
Glitch (hello-express) <none>
2021-05-16 10:39:17 +0000

    🍏👧 Checkpoint ./src/command/list/moderation/config.js:31004912/67 ./src/command/list/moderation/kick.js:31004912/428

---
+++ src/command/list/moderation/kick.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "help",
+  description: "Show all existing commands",
+  aliases: ["h"],
+  permissions: {
+    user: ["KICK_MEMBERS"],
+    client: []
+  },
+  cooldown: 5,
+  run: async (message, args, client) => {
+    
+    
+    
+  }
+}
