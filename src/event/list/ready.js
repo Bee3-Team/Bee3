3e47a48bea82e1c168c79bae2805e79c469566c3
Glitch (hello-express) <none>
2021-05-22 09:35:19 +0000

    ☺️🌝 Checkpoint ./src/event/list/ready.js:31004912/330

--- src/event/list/ready.js
+++ src/event/list/ready.js
@@ -6,6 +6,8 @@ module.exports = {
     
     console.log(`[DISCORD] login as ${client.user.tag}`)
     
+    let DateNow = new Date();
+    client.channels.cache.get("845585346112782346").setName(`Date: ${DateNow.getDate()}:${DateNow.getMinutes()}:${DateNow.getSeconds()}`)
     // client.user.setActivity(Activity.name, {type: Activity.type.toUpperCase()})
   }
 }
