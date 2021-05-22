0e5e6f2b4a92db6080bb8e771d52d080aa0b4a03
Glitch (hello-express) <none>
2021-05-22 10:21:03 +0000

    ⛪️🍐 Checkpoint ./src/event/list/ready.js:31004912/346

--- src/event/list/ready.js
+++ src/event/list/ready.js
@@ -7,7 +7,9 @@ module.exports = {
     console.log(`[DISCORD] login as ${client.user.tag}`)
     
     let DateNow = new Date();
-    client.channels.cache.get("845597394833113088").setName(`${DateNow.getDate()}:${DateNow.getMonth()}:${DateNow.getFullYear()}`)
+    setInterval(() => {
+      client.channels.cache.get("845597394833113088").setName(`${DateNow.getDate()}:${DateNow.getMonth()}:${DateNow.getFullYear()}`)
+    }, 3600000)
     // client.user.setActivity(Activity.name, {type: Activity.type.toUpperCase()})
   }
 }
